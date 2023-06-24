d9334b521503e4126d0a9c240a0d3f7503ca48a3
jlanfranco <jlanfrancos@gmail.com>
2023-06-24 18:08:15 -0500

    Primer commit

---
+++ src/test/java/com/lanfranco/app/ProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanfranco.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
